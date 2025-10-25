7dfc5087824dd3d8127396ef70822bffad8ed902
iyiinsan5555 <mhbsonmez@gmail.com>
2025-10-25 13:18:58 +0300

    Stayed at Event Loop 2:27:38

--- app.js
+++ app.js
@@ -2,4 +2,14 @@
 // pip = Package Installer Python
 
 
-//Stayed at Share Code --> 2:02:53
+//Stayed at Share Code --> 2:02:53'
+
+for (i=0; i<5;i++) {
+    for (j=0; j<i;j++) {
+        string = "*" 
+        console.log(string.repeat(j));
+    }
+    console.log("");
+}
+
+//Stayed at Event Loop 2:27:38
