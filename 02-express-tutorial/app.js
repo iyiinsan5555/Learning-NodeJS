aeda922e86a89c88cd5b75ad388cffbf4350f692
iyiinsan5555 <mhbsonmez@gmail.com>
2025-10-27 10:21:37 +0300

    Gave a break -->Stayed at 4:24:50 (Http - Request Object)

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,1 +1,35 @@
 console.log('Express Tutorial')
+
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+    console.log("server got request");
+    
+    res.writeHead(200,{"content-type": "text/html"})
+
+    html = `
+    <div style="display: flex; align-items: center; justify-content: center; width: 100vw; height: 100vh;">
+        <h1>Hello, world!</h1>
+    </div>
+    `;
+
+    html404 = `
+    <div style="display: flex; align-items: center; justify-content: center; width: 100vw; height: 100vh;">
+        <h1>Page not found!</h1>
+    </div>
+    `;
+
+    res.write(html, (err) => {
+        console.log(err + " error");
+    })
+
+    if (res.statusCode === 404 ) {
+        res.write(html404);
+    }
+    res.end();
+
+    console.log(res.statusCode)
+    console.log(res.statusMessage)
+});
+
+server.listen(2000);
